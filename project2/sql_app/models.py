27fcfbadd308923643bcd5b8c92af1fd02992ebd
Rishabh <rishabhtripathi0202@gmail.com>
2023-09-17 21:59:31 +0530

    Second

---
+++ project2/sql_app/models.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from sqlalchemy import Boolean,Integer,String,Column,ForeignKey
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
